05cd018657ddd34b9be058e94873a2409e61bc14
TianoRao <rtj0808@outlook.com>
2023-11-19 12:54:31 -0500

    Update

--- main.py
+++ main.py
@@ -1,19 +1,65 @@
-"""
-Main cli or app entry point
-"""
+"""runs a simple machine learning expirement"""
+import mlflow
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+from mlflow.models import infer_signature
 
-from mylib.calculator import add
-import click
 
-#var=1;var=2
+def main():
+    """runs a basic logistic regression model
+    and logs it with mlflow"""
+    X, y = datasets.load_iris(return_X_y=True)
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # Define the model hyperparameters
+    params = {
+        "solver": "lbfgs",
+        "max_iter": 1000,
+        "multi_class": "auto",
+        "random_state": 8888,
+    }
+
+    # Train the model
+    lr = LogisticRegression(**params)
+    lr.fit(X_train, y_train)
+
+    # Predict on the test set
+    y_pred = lr.predict(X_test)
+
+    # Calculate accuracy as a target loss metric
+    accuracy = accuracy_score(y_test, y_pred)
+
+    with mlflow.start_run():
+        # Log the hyperparameters
+        mlflow.log_params(params)
+
+        # Log the loss metric
+        mlflow.log_metric("accuracy", accuracy)
+
+        # Set a tag that we can use to remind ourselves what this run was for
+        mlflow.set_tag("Training Info", "Basic LR model for iris data")
+
+        # Infer the model signature
+        signature = infer_signature(X_train, lr.predict(X_train))
+
+        # Log the model
+        model_info = mlflow.sklearn.log_model(
+            sk_model=lr,
+            artifact_path="iris_model",
+            signature=signature,
+            input_example=X_train,
+            registered_model_name="tracking-quickstart",
+        )
+
+    loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
+
+    return loaded_model
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    main()
